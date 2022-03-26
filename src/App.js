505944dac332cdc9cdc44191c29a22dee1540ea4
MaksMatch <maksim.matchenia@gmail.com>
2022-03-26 16:07:15 +0100

    add registration page for pacient

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LoginDoctot from "./pages/doctor/login/LoginDoctor";
 import LoginPacient from "./pages/pacient/login/LoginPacient";
+import RegistrationPacient from "./pages/pacient/registration/RegistrationPacient";
 
 function App() {
   return (
     <Routes>
       <Route path="/pacient/login" element={<LoginPacient />} />
       <Route path="/doctor/login" element={<LoginDoctot />} />
+      <Route path="/pacient/sign_up" element={<RegistrationPacient />} />
     </Routes>
   );
 }
