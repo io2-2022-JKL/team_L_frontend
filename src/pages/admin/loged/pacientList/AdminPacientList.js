2623268f047c6746c9624bc9f64c3b75436eb0cb
MaksMatch <maksim.matchenia@gmail.com>
2022-04-10 01:41:18 +0200

    pacient list

--- src/pages/admin/loged/pacientList/AdminPacientList.js
+++ src/pages/admin/loged/pacientList/AdminPacientList.js
@@ -1,87 +1,72 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Table } from "react-bootstrap";
 
-export class AdminPacientList extends Component{
+function AdminPacientList() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [deps, setLoadedMeetups] = useState([]);
 
-    constructor(props){
-        super(props);
-        this.state = {
-            deps:[],
-            isLoaded: false,
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://virtserver.swaggerhub.com/01151586/VaccinationSystem/2.0.0/admin/patients"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = { id: key, ...data[key] };
+          meetups.push(meetup);
         }
-    }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
-    refreshList(){
-        fetch('https://virtserver.swaggerhub.com/01151586/VaccinationSystem/2.0.0/admin/patients',
-        {
-            method:'GET',
-        })
-        .then(response=>response.json())
-        .then(data=>{
-            this.setState({
-                deps:data,
-                isLoaded:true,
-            })
-        });
-    }
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
-    componentDidMount(){
-        this.refreshList();
-    }
+  return (
+    <div>
+      <div className="mt-2 d-flex justify-content-center">
+        lista z pacjentami
+      </div>
 
-    componentDidUpdate(){
-        this.refreshList();
-    }
-
-    render(){
-        const {deps,isLoaded} = this.state;
-
-
-        if(!isLoaded){
-            return(
-                <div>Loading...</div>
-            )
-
-        }
-
-        return(
-            <div>
-                <div className="mt-2 d-flex justify-content-center">
-                    lista z pacjentami
-                </div>
-
-                <Container>
-                <div >
-                    <Table className="mt-4" hover striped bordered size="sm">
-                        <thead>
-                            <tr>
-                                <th>Imie</th>
-                                <th>Nazwisko</th>
-                                <th>mail</th>
-                                <th>Telefon</th>
-                                <th>Opcje</th>
-                            </tr>
-
-                            
-                        </thead>
-                        <tbody>
-                            {deps.map(dep=>
-                                    <tr key={dep.firstName}>
-                                        <td>{dep.firstName}</td>
-                                        <td>{dep.lastName}</td>
-                                        <td>{dep.mail}</td>
-                                        <td>{dep.phoneNumber}</td>
-                                        <td>Edycja / usuwanie</td>
-                                    </tr>
-                                    
-                                    )}
-                        </tbody>
-                    </Table>
-                </div>
-                </Container>
-            </div>
-        )
-
-    }
+      <Container>
+        <div>
+          <Table className="mt-4" hover striped bordered size="sm">
+            <thead>
+              <tr>
+                <th>Imie</th>
+                <th>Nazwisko</th>
+                <th>mail</th>
+                <th>Telefon</th>
+                <th>Opcje</th>
+              </tr>
+            </thead>
+            <tbody>
+              {deps.map((dep) => (
+                <tr key={dep.firstName}>
+                  <td>{dep.firstName}</td>
+                  <td>{dep.lastName}</td>
+                  <td>{dep.mail}</td>
+                  <td>{dep.phoneNumber}</td>
+                  <td>Edycja / usuwanie</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
+export default AdminPacientList;
