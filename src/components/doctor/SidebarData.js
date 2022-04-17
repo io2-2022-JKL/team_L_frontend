2e67b9c37c04377c2938eb80d1b75f1c7e25dd7c
MaksMatch <maksim.matchenia@gmail.com>
2022-04-17 15:43:17 +0200

    navigation for doctor + fix bugs

---
+++ src/components/doctor/SidebarData.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as GiIcons from "react-icons/gi";
+import * as AiIcons from "react-icons/ai";
+
+export const sidebarData = [
+  {
+    title: "Home",
+    path: "",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Former Appointments",
+    path: "formerAppointments",
+    icon: <GiIcons.GiLoveInjection />,
+    cName: "nav-text",
+  },
+];
--- src/pages/doctor/logged/doctorLogged.js
+++ src/pages/doctor/logged/doctorLogged.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
-import { DoctorNavigation } from "../navigator/doctorNavigation";
-import { DoctorPatientList } from "./patientList/DoctorPatientList";
+import DoctorAppointmentList from "./appointmentList/DoctorAppointmentList";
+import Navbar from "../../../components/Navbar";
 import styles from "./doctorLogged.module.css";
+import { sidebarData } from "../../../components/doctor/SidebarData";
 
 export default class DoctorLogged extends Component {
   render() {
     return (
       <div className={styles.divset}>
-        <DoctorNavigation />
+        <Navbar sidebarData={sidebarData} />
         <Routes>
-          <Route path="/formerAppointmets" element={<DoctorPatientList />} />
+          <Route
+            path="/formerAppointments"
+            element={<DoctorAppointmentList />}
+          />
         </Routes>
       </div>
     );
