8ee9aaa246f18a2252c32ff4874bbad6cf838a31
MaksMatch <maksim.matchenia@gmail.com>
2022-04-21 02:04:15 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/Login";
 import AdminLogged from "./pages/admin/loged/adminLogged";
 import RegistrationPatient from "./pages/patient/registration/RegistrationPatient";
@@ -13,22 +13,26 @@ function App() {
     <Routes>
       {!Auth.isUserLogged() && (
         <>
+          <Route path="/" element={<Navigate to="/login" />}></Route>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/patient/sign_up" element={<RegistrationPatient />} />
         </>
       )}
       {Auth.isUserAccessRole("patient") && (
         <>
+          <Route path="/" element={<Navigate to="/patient/" />}></Route>
           <Route path="/patient/*" element={<PatientLogged />} />
         </>
       )}
       {Auth.isUserAccessRole("doctor") && (
         <>
+          <Route path="/" element={<Navigate to="/doctor/" />}></Route>
           <Route path="/doctor/*" element={<DoctorLogged />} />
         </>
       )}
       {Auth.isUserAccessRole("admin") && (
         <>
+          <Route path="/" element={<Navigate to="/admin/" />}></Route>
           <Route path="/admin/*" element={<AdminLogged />} />
         </>
       )}
