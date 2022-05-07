11ca71984354998ea4bdadc94d543987407be35e
MaksMatch <maksim.matchenia@gmail.com>
2022-05-07 16:09:03 +0200

    patient info

---
+++ src/components/shared/ProfileDataCard.js
@@ -0,0 +1,21 @@
+function ProfileDataCard(props) {
+  return (
+    <div>
+      <h5>Full Name:</h5>
+      <p>
+        {props.loadedPatient.firstName} {props.loadedPatient.lastName}
+      </p>
+      <h5>Email:</h5>
+      <p>{props.loadedPatient.mail}</p>
+      <h5>PESEL:</h5>
+      <p>{props.loadedPatient.PESEL}</p>
+      <h5>Date of Birth:</h5>
+      <p>{props.loadedPatient.dateOfBirth}</p>
+      <h5>Phone Number:</h5>
+      <p>{props.loadedPatient.phoneNumber}</p>
+      {props.children}
+    </div>
+  );
+}
+
+export default ProfileDataCard;
---
+++ src/pages/patient/logged/home/home.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import ProfileDataCard from "../../../../components/shared/ProfileDataCard";
+import { basicURL } from "../../../../Services";
+import Auth from "../../../../services/Auth";
+
+function Home() {
+  const [loadedPatient, setLoadedPatient] = useState({});
+
+  async function fetchingData() {
+    const userId = Auth.getUserId();
+    const response = await fetch(basicURL + "/patient/info/" + userId);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      setLoadedPatient(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  return (
+    <div>
+      <Container className="mt-4">
+        <h2 className="mb-3">Profile Data</h2>
+        <ProfileDataCard loadedPatient={loadedPatient} />
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
