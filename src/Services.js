acef89fffa0856510aa1935450dec03f23528b71
MaksMatch <maksim.matchenia@gmail.com>
2022-05-26 00:47:39 +0200

    services

--- src/Services.js
+++ src/Services.js
@@ -1,6 +1,6 @@
 // export const basicURL = "https://localhost:5001";
 // Our
-export const basicURL = "https://teamlvaccinationsystem.azurewebsites.net";
+export const basicURL = "https://vaccinationsystemteaml.azurewebsites.net";
 // Team J
 // export const basicURL = "https://systemszczepien.azurewebsites.net";
 // Team K
