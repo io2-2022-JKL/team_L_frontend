97845294aea2e2979409d0d8a217aea2d1a0a3cc
MaksMatch <maksim.matchenia@gmail.com>
2022-06-11 19:23:51 +0200

    other fixes

--- src/Services.js
+++ src/Services.js
@@ -1,10 +1,10 @@
 // export const basicURL = "https://localhost:5001";
 // Our
-export const basicURL = "https://vaccinationsystemteaml.azurewebsites.net";
+// export const basicURL = "https://vaccinationsystemteaml.azurewebsites.net";
 // Team J
 //export const basicURL = "https://systemszczepien.azurewebsites.net";
 // Team K
-// export const basicURL = "https://vaccinationsystemapi.azurewebsites.net";
+export const basicURL = "https://vaccinationsystemapi.azurewebsites.net";
 // Swagger
 // export const basicURL =
 //   "https://virtserver.swaggerhub.com/01151586/VaccinationSystem/3.0.0";
